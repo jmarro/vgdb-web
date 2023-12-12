fcb86fdba6b5ad5f6b8498073c661db71f969cf9
jmarro <juanmf86@gmail.com>
2023-12-12 17:54:06 +0100

    game detail scaffolding

--- src/app/pages/games/game-detail/game-detail.page.ts
+++ src/app/pages/games/game-detail/game-detail.page.ts
@@ -6,11 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrl: './game-detail.page.scss'
 })
 export class GameDetailPage implements OnInit {
+  public cover: boolean = true;
+  public owned: boolean = false;
+
+
   constructor() {
+
   }
+
+  
 
   ngOnInit() {
     console.log('gamesDetail!');
+    this.cover = true;
+  }
+
+  public gameOwned(e: any) {
+    console.log(e);
   }
 
 }
--- src/app/pages/games/games.module.ts
+++ src/app/pages/games/games.module.ts
@@ -1,11 +1,14 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatChipsModule } from '@angular/material/chips';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 import { GamesPage } from './games.page';
 import { GAMES_ROUTES } from './games-routes.page';
 import { GameDetailPage } from './game-detail/game-detail.page';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,7 +17,10 @@ import { GameDetailPage } from './game-detail/game-detail.page';
   ],
   imports: [
     CommonModule,
+    MatButtonToggleModule,
     MatChipsModule,
+    MatTooltipModule,
+    FormsModule,
     RouterModule.forChild(GAMES_ROUTES)
   ],
   schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
